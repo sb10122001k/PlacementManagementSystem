7403bbec0c793bae3ed31b9032c62efd61f28853
msprajwal <msprajwal2001@gmail.com>
2023-05-11 13:14:34 +0530

    database is working

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,148 @@
 const express=require('express')
 const cors = require('cors')
 const app = express()
- 
+const dotenv = require('dotenv')
+const {Student, Company, Placement} = require('./models')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+dotenv.config()
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connection open");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+} 
 app.use(cors())
 app.use(express.json())
 
-app.post('/api/studentLogin',(req,res)=>{
-    console.log(req.body)
+app.post('/api/studentRegister', async (req, res)=>{
+
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        
+        const newStudent = await Student.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            usn: req.body.usn,
+            email: req.body.email,
+            password: hashedPassword,
+    
+        })
+
+        const student = await newStudent.save()
+        res.status(200).json(student) 
+    } catch(err){
+        console.log(err);
+    }
+})
+
+app.post('/api/studentLogin',async (req,res)=>{
+   
+    try{
+        const student = await Student.findOne({usn: req.body.usn})
+        !student && res.status(404).json("student not found")
+
+        const validPassword = await bcrypt.compare(req.body.password, student.password)
+        !validPassword && res.status(400).json("invalid password")
+
+        res.status(200).json(student)
+    } catch(err){
+        console.log(err);
+    }
     // res.status(206).send("ok")
 })
-app.post('/api/registerCompany',(req,res)=>{
-    console.log(req.body)
+
+app.put('/api/studentUpdate', async (req, res) => {
+    const { usn, skills, class10, class12, dateOfBirth} = req.body;
+  
+    try {
+      const updatedStudent = await Student.findOneAndUpdate(
+        { usn },
+        { skills , dateOfBirth,
+        class10: class10,
+        class12: class12},
+        { new: true }
+      );
+  
+      if (!updatedStudent) {
+        return res.status(404).json({ message: 'Student not found' });
+      }
+  
+      // Save the updated document
+      await updatedStudent.save();
+  
+      res.json(updatedStudent);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+app.post('/api/registerCompany',async (req,res)=>{
+    
+    
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        
+        const newCompany = await Company.create({
+            
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+                address: req.body.address,
+                website: req.body.website,
+                contact: req.body.contact,
+              
+
+        })
+
+        const company = await newCompany.save()
+        res.status(200).json(company) 
+    } catch(err){
+        console.log(err);
+    }
+
     // res.status(206).send("ok")
 })
-app.post('/api/companyLogin',(req,res)=>{
+app.post('/api/companyLogin',async (req,res)=>{
     console.log(req.body)
+    try{
+        const company = await Company.findOne({email: req.body.email})
+        !company && res.status(404).json("Company not found")
+
+        const validPassword = await bcrypt.compare(req.body.password, company.password)
+        !validPassword && res.status(400).json("invalid password")
+
+        res.status(200).json(company.name)
+    } catch(err){
+        console.log(err);
+    }
 })
+
+app.put('/api/companyUpdate', async (req, res) => {
+    const {name, email, password, address, website, contact} = req.body;
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    try {
+      const updatedCompany = await Company.findOneAndUpdate(
+        { email },
+        {name, hashedPassword, address, website, contact},
+        { new: true }
+      );
+  
+      if (!updatedCompany) {
+        return res.status(404).json({ message: 'Company not found' });
+      }
+  
+      // Save the updated document
+      await updatedCompany.save();
+  
+      res.json(updatedCompany);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 app.post('/api/registerAdmin',(req,res)=>{
     console.log(req.body)
 })
---
+++ backend/models.js
@@ -0,0 +1,124 @@
+const mongoose = require('mongoose');
+
+//student schema
+const StudentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  usn: {
+    type: String,
+    required: true
+  },
+  skills: [
+    {
+      type: String
+    }
+  ],
+  dateOfBirth: {
+    type: String,
+    default: Date.now
+  },
+  class10: {
+    schoolName: {
+      type: String,
+      required: true
+    },
+    board: String,
+    marks: String,
+    passingYear: Number
+  },
+  class12: {
+    schoolName: {
+      type: String,
+      required: true
+    },
+    board: String,
+    marks: String,
+    passingYear: Number
+  }
+});
+//company schema
+const CompanySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: String
+  },
+  contact: {
+    email: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    }
+  }
+});
+
+
+const PlacementSchema = new mongoose.Schema({
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'company',
+    required: true
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  requirements: [{
+    type: String
+  }],
+  deadline: {
+    type: Date,
+    required: true
+  },
+  appliedStudents: [{
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'student'
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    status: {
+      type: String,
+      enum: ['applied', 'shortlisted', 'rejected', 'offered'],
+      default: 'applied'
+    }
+  }]
+});
+
+// Export the models
+module.exports = {
+  Student: mongoose.model('student', StudentSchema),
+  Company: mongoose.model('company', CompanySchema),
+  Placement: mongoose.model('placement', PlacementSchema)
+};
